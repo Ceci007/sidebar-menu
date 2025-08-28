8688390e984c9ec7d4529f42b003ed2ab7362835
UnicornDad8 <cecibenitezca@gmail.com>
2025-08-28 17:43:15 -0300

    Open and close sidebar menu

--- script.js
+++ script.js
@@ -6,6 +6,17 @@ const body = document.querySelector("body"),
       modeText = body.querySelector(".mode-text");
 
 
+
+      toggle.addEventListener("click", () => {
+        sidebar.classList.toggle("close");
+      })
+
       modeSwitch.addEventListener("click", () => {
         body.classList.toggle("dark");
+
+        if(body.classList.contains("dark")) {
+          modeText.innerText = "Light Mode";
+        } else {
+          modeText.innerText = "Dark Mode";
+        }
       })
